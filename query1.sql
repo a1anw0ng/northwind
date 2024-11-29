2692b6e957235fc56a69a8665beceb1b2bc869b2
alanw0ng <alanjw8@gmail.com>
2024-11-28 20:38:11 -0800

    basic query

---
+++ query1.sql
@@ -0,0 +1,27 @@
+select * from categories;
+
+select * from customer_customer_demo;
+
+select * from customer_demographics;
+
+select * from customers;
+
+select * from employee_territories;
+
+select * from employees;
+
+select * from order_details;
+
+select * from orders;
+
+select * from products;
+
+select * from region;
+
+select * from shippers;
+
+select * from suppliers;
+
+select * from territories;
+
+select * from us_states;
